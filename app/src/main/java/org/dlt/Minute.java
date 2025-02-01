2e1000d7b624e73f7b6d850e03c8d7f486bf2907
lengpiseth <lengpiseth@gmail.com>
2025-02-01 20:02:12 +0700

    update template docx add footer text replacement

--- app/src/main/java/org/dlt/Minute.java
+++ app/src/main/java/org/dlt/Minute.java
@@ -3,6 +3,7 @@ package org.dlt;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFFooter;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.dlt.model.Enterprise;
@@ -56,25 +57,27 @@ class Minute {
 
         try (FileInputStream fis = new FileInputStream(inputFile);
              XWPFDocument docx = new XWPFDocument(fis);) {
-            logger.info("Reading DOCX file = " + inputFile);
+//            logger.info("Reading DOCX file = " + inputFile);
 
             for (int i = 0; i < placeholders.length; i++) {
                 String searchStr = placeholders[i];
                 String replaceStr = replacements[i];
 
-                logger.info("placeholder=" + searchStr);
+                logger.info("PLACEHOLDER LOOPER=" + searchStr);
 
                 for (XWPFParagraph paragraph : docx.getParagraphs()) {
                     String text = paragraph.getText();
 
-                    logger.info("text=" + text);
+//                    logger.info("text=" + text);
 
                     if (text.contains(searchStr)) {
                         for (XWPFRun run : paragraph.getRuns()) {
                             String runText = run.getText(0);
-                            logger.info("runText=" + runText);
+
+//                            logger.info("runText=" + runText);
+
                             if (runText != null && runText.contains(searchStr)) {
-                                logger.info("replacement=" + replaceStr);
+//                                logger.info("replacement=" + replaceStr);
                                 runText = runText.replace(searchStr, replaceStr);
                                 run.setText(runText, 0);
                                 run.setBold(true);
@@ -82,11 +85,35 @@ class Minute {
                         }
                     }
                 }
+
+                int f=0;
+                // Need to loop throw 3 footers or MORE, but normally 3
+                // 1. Section-based
+                // 2. Even and odd pages
+                // 3. First-page footer
+                // 4. Regular Section Footer (not the above 3)
+                for (XWPFFooter footer : docx.getFooterList()) {
+                    logger.info("footer# "+f);
+                    for (XWPFParagraph footerParagraph : footer.getParagraphs()) {
+                        for (XWPFRun footerRun : footerParagraph.getRuns()) {
+                            String footerRunText = footerRun.getText(0);
+                            logger.info("footerTEXT=" + footerRunText);
+                            if (footerRunText != null && footerRunText.contains(searchStr)) {
+                                footerRunText = footerRunText.replace(searchStr, replaceStr);
+                                footerRun.setText(footerRunText, 0);
+                            }
+                        }
+                    }
+                    f++;
+                }
             }
 
             try (FileOutputStream fos = new FileOutputStream(outputFile)) {
-                docx.write(fos);
                 logger.info("Writing DOCX file = " + outputFile);
+                docx.write(fos);
+
+                fis.close();
+                docx.close();
             } catch (IOException e) {
                 logger.error(e.getMessage());
             }
